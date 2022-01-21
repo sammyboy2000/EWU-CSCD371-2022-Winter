ced698e91a72af0182ffd1dba9e5916227efdf9c
Samuel Shaw <samuelshaw2000@gmail.com>
2022-01-21 11:47:39 -0800

    Created FileLogger from BaseLogger. Created initialization method for FileLogger. Added using System.IO. Added to Log.

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+        public string? FilePath { get; set; }
+        //string? ClassName inherited from BaseLogger
+        public FileLogger(string className, string filePath)
+        {
+            ClassName = className;
+            FilePath = filePath;
+        }
+
+        public override void Log(LogLevel logLevel, string message)
+        {
+            //example: 10/7/2019 12:38:59 AM FileLoggerTests Warning: Test message
+            File.AppendAllText(FilePath, $"{DateTime.Now} {nameof(ClassName)} {logLevel}: {message}");
+        }
+    }
+}
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,10 +1,24 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public class LogFactory
     {
         public BaseLogger CreateLogger(string className)
         {
-            return null;
+            //Unfinished
+            return ConfigureFileLogger(className, Console.ReadLine());
+        }
+
+        public BaseLogger ConfigureFileLogger(string className, string filePath)
+        {
+            if (String.IsNullOrEmpty(filePath))
+            {
+                Console.WriteLine("File path cannot be empty or null");
+                return null!;
+            }
+
+            return new FileLogger(className, filePath);
         }
     }
 }
