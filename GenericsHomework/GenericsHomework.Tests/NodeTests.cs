56124a6f531209272c8d56d1769d93c22c9e97ee
Samuel Shaw <samuelshaw2000@gmail.com>
2022-02-05 16:53:02 -0800

    Created Node class. Created ToString(). Created Append() Created Exists() Created relavent tests.

--- GenericsHomework/GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -6,9 +6,46 @@ namespace GenericsHomework.Tests
     public class NodeTests
     {
         [TestMethod]
-        public void Node_ReturnsValidNode()
+        public void Node_ReturnsNode()
         {
-
+            Node node = new(null);
+            Assert.IsNotNull(node);
+        }
+        [TestMethod]
+        public void ToString_ProperlyParses()
+        {
+            Node node = new(null);
+            Assert.AreEqual("null", node.ToString());
+            node.Value = "test";
+            Assert.AreEqual("test", node.ToString());
+            node.Value = false;
+            Assert.AreEqual("false", node.ToString());
+            node.Value = 39;
+            Assert.AreEqual("39", node.ToString());
+            node.Value = (Int64)64;
+            Assert.AreEqual("64", node.ToString());
+            node.Value = 42.1;
+            Assert.AreEqual("42.1", node.ToString());
+            node.Value = new Node(null);
+            Assert.AreEqual("Type unknown", node.ToString());
+        }
+        [TestMethod]
+        public void Append_ProperlyAppends()
+        {
+            Node node = new("1st");
+            node.Append("2nd");
+            node.Append("3rd");
+            Assert.AreEqual("1st", node.ToString());
+            Assert.AreEqual("2nd", node.GetNext().ToString());
+            Assert.AreEqual("3rd", node.GetNext().GetNext().ToString());
+            Assert.AreEqual("1st", node.GetNext().GetNext().GetNext().ToString());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]  
+        public void Append_ItemExistsException()
+        {
+            Node node = new("null");
+            node.Append(null);
         }
     }
 }
--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -2,6 +2,71 @@
 {
     public class Node
     {
-
+        public object? Value { get; set; }
+        private Node Next { get;  set; }
+        public Node GetNext() { return Next; }
+        public Node(object? value)
+        {
+            Value = value;
+            Next = this;
+        }
+        public override string ToString()
+        {
+            if (Value == null)  
+                return "null";
+            Type type = Value.GetType();
+            if(type == typeof(string) || type == typeof(char))
+            {
+                return (string)Value;
+            }
+            if(type == typeof(bool))
+            {
+                bool b = (bool)Value;
+                if (b)
+                    return "true";
+                else
+                    return "false";
+            }
+            if(type == typeof(int))
+            {
+                return $"{(int)Value}";
+            }
+            if(type == typeof(Int64))
+            {
+                return $"{(Int64)Value}";
+            }
+            if(type == typeof(float) || type == typeof(Double))
+            {
+                return $"{(Double)Value}";
+            }
+            return "Type unknown";
+        }
+        public void Append(object? value)
+        {
+            if (Exists(value))
+                throw new ArgumentException("Value already exists in list.");
+            Node index = this;
+            while (Next != index)
+            {
+                index = index.Next;
+            }
+            index.Next = new Node(value);
+            index = index.Next;
+            index.Next = this;
+        }
+        public bool Exists(object? Value)
+        {
+            Node temp = new(Value);
+            Node index = this;
+            if(temp.ToString() == index.ToString())
+                return true;
+            while(index.Next != this)
+            {
+                if (temp.ToString() == index.ToString())
+                    return true;
+                index = index.Next;
+            }
+            return false;
+        }
     }
 }
