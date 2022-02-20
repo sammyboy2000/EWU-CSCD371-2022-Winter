e17e38661d36580917325e718b67da54d1d00f15
Samuel Shaw <samuelshaw2000@gmail.com>
2022-02-19 23:28:59 -0800

    Added tests for GetUniqueSortedListOfStatesGivenCsvRows() and GetAggregateSortedListOfStatesUsingCsvRows(). Added test method to check for uniqueness. Implemented People property and test. Implemented FilterByEmailAddress. Added GetAggregateListOfStatesGivenPeopleCollection. Added relevent tests.

--- Assignment/Assignment.Tests/AssignmentTests.cs
+++ Assignment/Assignment.Tests/AssignmentTests.cs
@@ -31,5 +31,96 @@ namespace Assignment.Tests
             }
             Assert.AreEqual<int>(51-1, count);
         }
+
+        [TestMethod]
+        public void GetUniqueSortedListOfStatesActuallyUnique()
+        {
+            SampleData sampleData = new();
+            IEnumerable<string> dataSet = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
+            foreach(string data in dataSet)
+            {
+                Assert.IsFalse(ExistsMoreThanOnce(dataSet,data));
+            }
+        }
+        [TestMethod]
+        public void GetUniqueSortedListOfStatesActuallySorted()
+        {
+            string highest = " ";
+            SampleData sampleData = new();
+            IEnumerable<string> data = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
+            foreach(string d in data)
+            {
+                Assert.AreEqual<int>(1, string.Compare(d, highest));
+                Assert.IsFalse(ExistsMoreThanOnce(data, d));
+                highest = d;
+            }
+        }
+        public void GetUniqueSortedListOfStatesActuallySortedString()
+        {
+            string highest = " ";
+            SampleData sampleData = new();
+            string result = sampleData.GetAggregateSortedListOfStatesUsingCsvRows();
+            IEnumerable<string> data = result.Split(",");
+            foreach (string d in data)
+            {
+                Assert.AreEqual<int>(1, string.Compare(d, highest));
+                Assert.IsFalse(ExistsMoreThanOnce(data, d));
+                highest = d;
+            }
+        }
+
+        [TestMethod]
+        public void PeopleContainsPersons()
+        {
+            SampleData sampleData = new();
+            IEnumerable<string> rows = sampleData.CsvRows;
+            int count = 0;
+            foreach (Person p in sampleData.People)
+            {
+                string person = rows.ElementAt(count);
+                string[] info = person.Split(",");
+                Assert.AreEqual<string>(info[1], p.FirstName);
+                Assert.AreEqual<string>(info[2], p.LastName);
+                Assert.AreEqual<string>(info[3], p.EmailAddress);
+                Assert.AreEqual<string>(info[4], p.Address.StreetAddress);
+                Assert.AreEqual<string>(info[5], p.Address.City);
+                Assert.AreEqual<string>(info[6], p.Address.State);
+                Assert.AreEqual<string>(info[7], p.Address.Zip);
+
+                count++;
+            }
+        }
+        [TestMethod]
+        public void FilterByEmailAddressSuccessful()
+        {
+            SampleData sampleData = new SampleData();
+            IEnumerable<(string firstName, string lastName)> results = sampleData.FilterByEmailAddress(new Predicate<string>(email => email == "pjenyns0@state.gov"));
+            Person person = (Person)sampleData.People.First();
+            foreach ((string fName, string lName) result in results)
+            {
+                Assert.AreEqual<string>(result.fName, person.FirstName);
+                Assert.AreEqual(result.lName, person.LastName);
+            }
+        }
+        [TestMethod]
+        public void GetAggregatedListOfStatesGivenPeopleCollectionProperlyUnique()
+        {
+            SampleData sampleData = new SampleData();
+            string result = sampleData.GetAggregateListOfStatesGivenPeopleCollection(null!);
+            Assert.AreEqual<string>(result, sampleData.GetAggregateSortedListOfStatesUsingCsvRows());
+        }
+        public static bool ExistsMoreThanOnce(IEnumerable<string> collection, string check)
+        {
+            int count = 0;
+            foreach(string existing in collection)
+            {
+                if (existing == check)
+                    count++;
+                if (count>1)
+                    return true;
+            }
+            return false;
+        }
     }
+
 }
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -12,32 +12,76 @@ namespace Assignment
         {
             get
             {
-                string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
                 string[] rows = File.ReadAllLines("People.csv");
                 IEnumerable<string> CsvRows =
                     rows.Skip(1);
                 return CsvRows;
             }
-
+            set
+            {
+            }
         }
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            IEnumerable<string> dataSet = CsvRows.Select(item =>
+            {
+                string[] split = item.Split(","); return split[6];
+            }).Distinct();
+            dataSet = from data in dataSet
+                      orderby data ascending
+                      select data;
+            return dataSet;
+        }
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+        {
+            IEnumerable<string> dataSet = GetUniqueSortedListOfStatesGivenCsvRows();
+            string[] data = dataSet.ToArray();
+            return String.Join(", ", data);
+
+        }
 
         // 4.
-        public IEnumerable<IPerson> People => throw new NotImplementedException();
+        public IEnumerable<IPerson> People
+        {
+            get
+            {
+                List<Person> people = new List<Person>();
+                foreach (string person in CsvRows)
+                {
+                    string[] info = person.Split(",");
+                    people.Add(new Person(info[1], info[2], new Address(info[4], info[5], info[6], info[7]), info[3]));
+                }
+                return people;
+            }
+            set
+            {
+            }
+        }
 
         // 5.
         public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
-            Predicate<string> filter) => throw new NotImplementedException();
+            Predicate<string> filter)
+        {
+            IEnumerable<IPerson> people = People.Where(person => filter(person.EmailAddress));
+            IEnumerable<(string, string)> results = people.Select(person => { return (person.FirstName, person.LastName); });
+            return results;
+        }
 
         // 6.
         public string GetAggregateListOfStatesGivenPeopleCollection(
-            IEnumerable<IPerson> people) => throw new NotImplementedException();
+            IEnumerable<IPerson> people)
+        {
+            if (people == null)
+                people = People;
+            IEnumerable<string> states = people.Select(people => people.Address.State)
+                .OrderBy(state => state)
+                .Distinct();
+            
+            return states.Aggregate((a, b) => a + ", " + b);
+        }
     }
 }
