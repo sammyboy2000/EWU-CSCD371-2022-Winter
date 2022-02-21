ddee8e90517bc124a86f2a478199bfc9b9ae8653
Samuel Shaw <samuelshaw2000@gmail.com>
2022-02-20 22:26:22 -0800

    Added Node from Assignment 4. Implemented IEnumerable<T> WholeList. Implemented ChildItems. Added tests for both.

---
+++ Assignment/Assignment.Tests/NodeTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Tests
+{
+    //Class copied from Assignment 4
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        public void Node_ReturnsNode()
+        {
+            Node<string> node = new(null!);
+            Assert.IsNotNull(node);
+        }
+        [TestMethod]
+        public void ToString_ProperlyParses()
+        {
+            Node<string> node = new("test");
+            Assert.AreEqual<string>("test", node.ToString()!);
+            Node<bool> node2 = new Node<bool>(false);
+            Assert.AreEqual<string>("False", node2.ToString()!);
+            Node<int> node3 = new(39);
+            Assert.AreEqual<string>("39", node3.ToString()!);
+            Node<double> node4 = new(42.1);
+            Assert.AreEqual<string>("42.1", node4.ToString()!);
+        }
+        [TestMethod]
+        public void Append_ProperlyAppends()
+        {
+            Node<string> node = new("1st");
+            node.Append("2nd");
+            node.Append("3rd");
+            Assert.AreEqual<string>("1st", node.ToString()!);
+            Assert.AreEqual<string>("2nd", node.Next.ToString()!);
+            Assert.AreEqual<string>("3rd", node.Next.Next.ToString()!);
+            Assert.AreEqual<string>("1st", node.Next.Next.Next.ToString()!);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Append_ItemExistsException()
+        {
+            Node<string> node = new("null");
+            node.Append("null");
+        }
+        [TestMethod]
+        public void Clear_ClearsList()
+        {
+            Node<string> node = new("1st");
+            node.Append("2nd");
+            node.Append("3rd");
+            node.Clear();
+            Assert.AreEqual(node, node.Next);
+        }
+
+        //New work begins here...
+        [TestMethod]
+        public void WholeList_ReturnsWholeList()
+        {
+            Node<string> node = new("1st");
+            node.Append("2nd");
+            node.Append("3rd");
+            string[] expectedResult = {"1st", "2nd", "3rd"};
+            int count = 0;
+            IEnumerable<string> list = (IEnumerable<string>)node.WholeList;
+            foreach(string value in list)
+            {
+                Assert.AreEqual(expectedResult[count++], value);
+            }
+        }
+        [TestMethod]
+        public void ChildItems_ReturnsCorrectAmountOfItems()
+        {
+            Node<string> node = new("1st");
+            node.Append("2nd");
+            node.Append("3rd");
+            string[] expectedResult = { "1st", "2nd" };
+            int count = 0;
+            IEnumerable<string> list = (IEnumerable<string>)node.ChildItems(2);
+            foreach (string value in list)
+            {
+                Assert.AreEqual(expectedResult[count++], value);
+            }
+        }
+    }
+}
+
---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    //Class copied from Assignment 4.
+    public class Node<T> where T : notnull
+    {
+        private T Value { get; set; }
+        public Node<T> Next { get; private set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = this;
+        }
+        public override string? ToString()
+        {
+            return Value.ToString();
+        }
+        public void Append(T value)
+        {
+            if (Exists(value))
+                throw new ArgumentException("Value already exists in list.");
+            Node<T> index = this;
+            while (Next != index)
+            {
+                index = index.Next;
+            }
+            index.Next = new Node<T>(value);
+            index = index.Next;
+            index.Next = this;
+        }
+        public bool Exists(T Value)
+        {
+            Node<T> temp = new(Value);
+            Node<T> index = this;
+            if (temp.ToString() == index.ToString())
+                return true;
+            do
+            {
+                if (temp.ToString() == index.ToString())
+                    return true;
+                index = index.Next;
+            } while (index != this);
+                return false;
+        }
+        public void Clear()
+        {
+            //Does work when simply setting this to this.Next
+            //GC is smart enough to clean up a circling list that has no references
+            //debug builds will hang onto that memory for longer though.
+            if (this == this.Next)
+                return;
+            Node<T> index = this;
+            while (index.Next != this)
+                index = index.Next;
+            index.Next = index.Next.Next;
+            this.Next = this;
+        }
+
+        //New work begins here...
+        public IEnumerable<T> WholeList 
+        {
+            get
+            { 
+                Node<T> index = this;
+                do
+                {
+                    yield return index.Value;
+                    index = index.Next;
+
+                }while (index != this);
+            } 
+        }
+        public IEnumerable<T> ChildItems(int maximum)
+        {
+            IEnumerable<T> temp = this.WholeList;
+            for(int i = 0; i < maximum; i++)
+            {
+                yield return temp.ElementAt(i);
+            }
+        }
+    }
+}
+
+
