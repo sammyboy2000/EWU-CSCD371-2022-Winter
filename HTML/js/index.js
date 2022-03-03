ac0282f0f82cc78c2376299e31d6a8b111bdbf4b
Samuel Shaw <samuelshaw2000@gmail.com>
2022-03-02 18:45:12 -0800

    Added Javascript and HTML
    Made Javascript functional.
    HTML skeleton is in place.
    More CSS.

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -1,14 +1,34 @@
+getJoke();
+
 function getJoke(){
     axios({
         method: 'get',
         url: 'https://v2.jokeapi.dev/joke/Programming'
     })
     .then(function(response){
-
+        clearPunchline();
+        if(response.data.type=="twopart")
+        {
+        $joke = response.data.setup;
+        $punchline = response.data.delivery;
+        document.getElementById("joke").innerText=$joke;
+        setTimeout(() => delayedPunchline($punchline), 4000);
+        }
+        else
+        {
+            document.getElementById("joke").innerText=response.data.joke;
+        }
     })
     .catch(function  (error){
-        document.getElementById()
+        document.getElementById("joke").innerText="Try again in a few seconds.";
     });
 }
-function displayJoke(){
+
+function delayedPunchline($punchline){
+    clearPunchline();
+    document.getElementById("punchline").innerText=$punchline;
+}
+
+function clearPunchline(){
+    document.getElementById("punchline").innerText="";
 }
